d3fdfa806b29864d8ed999cc463a0aa266b5204f
Coltar07 <148589118+Coltar07@users.noreply.github.com>
2024-10-17 15:56:36 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=Gy5hGCjL#ft2cJStcPBVOnnQ1L6j0ZHQz4lPu-FfuXhOkah_tcxA",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=7qZEnaYY#MFc9zOoakEA-J2vBoc4QYRVr1V2fDanyFKxJ42sQH9o"
 MONGODB: process.env.MONGODB || "mongodb+srv://Coltar07:<db_password>@coltar07.diwgx.mongodb.net//",//enter mongo db url
 };
